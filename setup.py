5503ed7448ffdd6ee60f90e3f3dacac11eb1c404
DevForcesfbis <160317951+DevForcesfbis@users.noreply.github.com>
2025-01-03 16:50:13 -0800

    setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
-git add .
-git commit -m "Initial commit"
-git push origin main
+from setuptools import setup, find_packages
+
+setup(
+    name='receiptgen',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+)
